169784bbb690488c95c940ce6c32ff0da08ddd3f
Wills <willsmckenna6@gmail.com>
2020-01-15 19:21:53 -0600

    first commits

---
+++ weather/views.py
@@ -0,0 +1,61 @@
+import requests
+from django.shortcuts import render, redirect
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=73de2245ecb4dd7220dbd1c1da3e776b'
+    err_msg = ''
+    message = ''
+    message_class = ''
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+        if existing_city_count == 0:
+            req = requests.get(url.format(new_city)).json()
+            if req['cod'] == 200:
+                form.save()
+            else:
+                err_msg = 'City does not exist'
+        else:
+            err_msg = "City already exists in the database!"
+        
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added succesfully'
+            message_class = 'is-success'
+        
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        req = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature' : req['main']['temp'],
+            'description' : req['weather'][0]['description'],
+            'icon' :  req['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+
+
+    return render(request, 'weather/home.html', 
+                            {'weather_data': weather_data,
+                             'form': form,
+                             'message' : message,
+                             'message_class' : message_class}
+                )   
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
